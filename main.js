6e1ff400ad0d373554e0fef9c4ee7f1a96bc1a52
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-04-19 16:28:25 -0500

    Works

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+    
+    document.querySelector('.theme-name').textContent = newTheme;
+  }
+  
+  document.querySelector('.theme-toggle').addEventListener('click', setTheme);
